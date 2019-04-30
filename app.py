d32553b68a50638f420eabbc4756f3df67bceffa
Amir <thelinkod@mail.ru>
2019-04-30 14:07:36 +0300

    Added POST requests and started working on admin panel

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import flask
+import uuid
 from database import *
 from playhouse.shortcuts import model_to_dict
 import json
@@ -40,6 +41,70 @@ def page_not_found_3(e):
     return resp(405, {})
 
 
+@app.route('/admin', methods=['GET'])
+def admin_panel():
+    return flask.render_template('admin.html')
+
+
+@app.route('/admin/water_communication', methods=['GET', 'POST'])
+def admin_water():
+    if flask.request.method == 'POST':
+        new_data = Water(id=uuid.uuid4(),
+                         type=flask.request.form['type'],
+                         owner=flask.request.form['owner'],
+                         start_coordinate_x=flask.request.form['start_coordinate_x'],
+                         start_coordinate_y=flask.request.form['start_coordinate_y'],
+                         end_coordinate_x=flask.request.form['end_coordinate_x'],
+                         end_coordinate_y=flask.request.form['end_coordinate_y'],
+                         depth=flask.request.form['depth'],
+                         work_info=flask.request.form['work_info'],
+                         work_date=flask.request.form['work_date'])
+        new_data.save(force_insert=True)
+    else:
+        waters = Water.select()
+        water_json = [model_to_dict(water) for water in waters]
+        return flask.render_template('water.html', data=water_json)
+
+
+@app.route('/admin/electricity_communication', methods=['GET', 'POST'])
+def admin_electricity():
+    if flask.request.method == 'POST':
+        new_data = Electricity(id=uuid.uuid4(),
+                               power=flask.request.form['power'],
+                               owner=flask.request.form['owner'],
+                               start_coordinate_x=flask.request.form['start_coordinate_x'],
+                               start_coordinate_y=flask.request.form['start_coordinate_y'],
+                               end_coordinate_x=flask.request.form['end_coordinate_x'],
+                               end_coordinate_y=flask.request.form['end_coordinate_y'],
+                               depth=flask.request.form['depth'],
+                               work_info=flask.request.form['work_info'],
+                               work_date=flask.request.form['work_date'])
+        new_data.save(force_insert=True)
+    else:
+        electirity = Electricity.select()
+        electirity_json = [model_to_dict(elem) for elem in electirity]
+        return flask.render_template('electricity.html', data=electirity_json)
+
+
+@app.route('/admin/gas_communication', methods=['GET', 'POST'])
+def admin_gas():
+    if flask.request.method == 'POST':
+        new_data = Gas(id=uuid.uuid4(),
+                       owner=flask.request.form['owner'],
+                       start_coordinate_x=flask.request.form['start_coordinate_x'],
+                       start_coordinate_y=flask.request.form['start_coordinate_y'],
+                       end_coordinate_x=flask.request.form['end_coordinate_x'],
+                       end_coordinate_y=flask.request.form['end_coordinate_y'],
+                       depth=flask.request.form['depth'],
+                       work_info=flask.request.form['work_info'],
+                       work_date=flask.request.form['work_date'])
+        new_data.save(force_insert=True)
+    else:
+        gas = Gas.select()
+        gas_json = [model_to_dict(elem) for elem in gas]
+        return flask.render_template('gas.html', data=gas_json)
+
+
 @app.route('/marker', methods=['GET'])
 def get_markers():
     x_pos, y_pos = get_user_position()
@@ -47,7 +112,7 @@ def get_markers():
                                     (Marker.coordinate_x > x_pos - X) &
                                     (Marker.coordinate_y < y_pos + Y) &
                                     (
-                                                Marker.coordinate_y > y_pos - Y)) if x_pos is not None and y_pos is not None else Marker.select()
+                                            Marker.coordinate_y > y_pos - Y)) if x_pos is not None and y_pos is not None else Marker.select()
     markers_json = [model_to_dict(marker) for marker in markers]
     return resp(200, markers_json)
 
@@ -69,7 +134,7 @@ def get_water_communication():
                                                 (Water.start_coordinate_x > x_pos - X) &
                                                 (Water.start_coordinate_y < y_pos + Y) &
                                                 (
-                                                            Water.start_coordinate_y > y_pos - Y)) if x_pos is not None and y_pos is not None else Water.select()
+                                                        Water.start_coordinate_y > y_pos - Y)) if x_pos is not None and y_pos is not None else Water.select()
     water_communications_json = [model_to_dict(water_communication) for water_communication in water_communications]
     return resp(200, water_communications_json)
 
@@ -91,7 +156,7 @@ def get_electricity_communication():
                                                     (Electricity.start_coordinate_x > x_pos - X) &
                                                     (Electricity.start_coordinate_y < y_pos + Y) &
                                                     (
-                                                                Electricity.start_coordinate_y > y_pos - Y)) if x_pos is not None and y_pos is not None else Electricity.select()
+                                                            Electricity.start_coordinate_y > y_pos - Y)) if x_pos is not None and y_pos is not None else Electricity.select()
     electricity_communications_json = [model_to_dict(electricity_communication) for electricity_communication in
                                        electricity_communications]
     return resp(200, electricity_communications_json)
@@ -114,7 +179,7 @@ def get_gas_communication():
                                     (Gas.start_coordinate_x > x_pos - X) &
                                     (Gas.start_coordinate_y < y_pos + Y) &
                                     (
-                                                Gas.start_coordinate_y > y_pos - Y)) if x_pos is not None and y_pos is not None else Gas.select()
+                                            Gas.start_coordinate_y > y_pos - Y)) if x_pos is not None and y_pos is not None else Gas.select()
     gas_communications_json = [model_to_dict(gas_communication) for gas_communication in gas_communications]
     return resp(200, gas_communications_json)
 
@@ -136,7 +201,7 @@ def get_data_communication():
                                       (Data.start_coordinate_x > x_pos - X) &
                                       (Data.start_coordinate_y < y_pos + Y) &
                                       (
-                                                  Data.start_coordinate_y > y_pos - Y)) if x_pos is not None and y_pos is not None else Data.select()
+                                              Data.start_coordinate_y > y_pos - Y)) if x_pos is not None and y_pos is not None else Data.select()
     data_communications_json = [model_to_dict(data_communication) for data_communication in data_communications]
     return resp(200, data_communications_json)
 
